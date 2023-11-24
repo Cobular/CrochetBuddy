6c1e4997a2d9c188554cca9c56e9bf3b4a9e3fa8
cobular <22972550+Cobular@users.noreply.github.com>
2023-11-23 16:40:32 -0800

    Added haptic feedback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,23 +15,29 @@
   
   swipeHandler.registerSwipeLeft(() => {
     console.log("swipe left");
-    if (progress > 0)
+    if (progress > 0) {
+      navigator.vibrate(50);
       progress -= 1;
+    }
   });
 
   swipeHandler.registerSwipeRight(() => {
     console.log("swipe right");
     if (parsed === null) return;
-    if (progress < parsed.total_stitches)
+    if (progress < parsed.total_stitches){
+      navigator.vibrate(30);
       progress += 1;
+    }
   });
 
-  swipeHandler.registerPress(() => {
-    console.log("press");
-    if (parsed === null) return;
-    if (progress < parsed.total_stitches)
-      progress += 1;
-  });
+  // swipeHandler.registerPress(() => {
+  //   console.log("press");
+  //   if (parsed === null) return;
+  //   if (progress < parsed.total_stitches){
+  //     navigator.vibrate(30);
+  //     progress += 1;
+  //   }
+  // });
 
   $: try {
     if ($text === "") {
@@ -71,7 +77,11 @@
   }
 </script>
 
-<svelte:document on:keydown={processKeyDown} on:touchstart={(e) => swipeHandler.onTouchStart(e)} on:touchend={(e) => swipeHandler.onTouchEnd(e)} />
+<svelte:document
+	on:keydown={processKeyDown}
+	on:touchstart={(e) => swipeHandler.onTouchStart(e)}
+	on:touchend={(e) => swipeHandler.onTouchEnd(e)}
+/>
 
 <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
 	<h1 class="text-4xl font-bold text-center text-blue-500 mb-5">Crochet Buddy</h1>
@@ -86,35 +96,39 @@
 	{/if}
 
 	<div class="flex items-center flex-col gap-2">
-		<div><kbd class="kbd kbd-sm">␣</kbd> to go next, <kbd class="kbd kbd-sm">←</kbd> to go back</div>
+		<div>
+			<kbd class="kbd kbd-sm">␣</kbd> to go next, <kbd class="kbd kbd-sm">←</kbd> to go back
+		</div>
 		<div><kbd class="kbd kbd-sm">r</kbd> to reset</div>
 	</div>
 	{#if parsed !== null}
-		<div class="mt-5 bg-gray-200 p-5 rounded-lg shadow-md flex flex-col items-center justify-center">
-      <div>
-        Overall progress: {progress} / {parsed.total_stitches}
-      </div>
-      <div class="flex flex-col">
-        {#each parsed.rows as row, i}
-          <div
-            class="grid gap-1"
-            style="grid-template-columns: repeat({rowLength(row)}, minmax(0, 1fr));"
-          >
-            {#each row as stitch, j}
-              <div
-                class="flex items-center justify-center border-b-2 border-gray-300"
-                style="grid-area: span 1 / span {stitch.num_stitches};"
-              >
-                {#each [...Array(stitch.num_stitches).keys()] as k}
-                  <span class="font-mono text-2xl"
-                    >{i * rowLength(parsed.rows[0]) + j + k < progress ? "x" : "_"}</span
-                  >
-                {/each}
-              </div>
-            {/each}
-          </div>
-        {/each}
-      </div>
-    </div>
+		<div
+			class="mt-5 bg-gray-200 p-5 rounded-lg shadow-md flex flex-col items-center justify-center"
+		>
+			<div>
+				Overall progress: {progress} / {parsed.total_stitches}
+			</div>
+			<div class="flex flex-col">
+				{#each parsed.rows as row, i}
+					<div
+						class="grid gap-1"
+						style="grid-template-columns: repeat({rowLength(row)}, minmax(0, 1fr));"
+					>
+						{#each row as stitch, j}
+							<div
+								class="flex items-center justify-center border-b-2 border-gray-300"
+								style="grid-area: span 1 / span {stitch.num_stitches};"
+							>
+								{#each [...Array(stitch.num_stitches).keys()] as k}
+									<span class="font-mono text-2xl"
+										>{i * rowLength(parsed.rows[0]) + j + k < progress ? "x" : "_"}</span
+									>
+								{/each}
+							</div>
+						{/each}
+					</div>
+				{/each}
+			</div>
+		</div>
 	{/if}
 </div>
